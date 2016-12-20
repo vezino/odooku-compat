5270e8ea24867af72214fdf5d7ad2de6558ff047
Raymond Reggers <raymond@Raymonds-MacBook-Pro.local>
2016-12-20 17:24:49 +0100

    wip

---
+++ addons/cdn/models/ir_qweb.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import odoo
+from odoo.osv import orm
+
+from odooku.s3 import pool as s3_pool
+from odooku.params import params
+
+CDN_ENABLED = getattr(params, 'CDN_ENABLED', False)
+
+
+class QWeb(orm.AbstractModel):
+
+    _inherit = 'ir.qweb'
+
+    CDN_TRIGGERS = {
+        'link':    'href',
+        'script':  'src',
+        'img':     'src',
+    }
+
+    def render_attribute(self, element, name, value, qwebcontext):
+        context = qwebcontext.context or {}
+        if CDN_ENABLED and not context.get('rendering_bundle') and s3_pool:
+            if name == self.CDN_TRIGGERS.get(element.tag):
+                parts = value.split('/')
+                cr, uid, context = [getattr(qwebcontext, attr) for attr in ('cr', 'uid', 'context')]
+                if value.startswith('/web/content/'):
+                    ira = self.pool['ir.attachment']
+                    domain = [('url', '=like', value)]
+                    attachment_ids = ira.search(cr, openerp.SUPERUSER_ID, domain, order='name asc', context=context)
+                    attachments = ira.browse(cr, openerp.SUPERUSER_ID, attachment_ids, context=context)
+                    if attachments:
+                        # /filestore/<dbname/<attachment>
+                        value = s3_pool.get_url('filestore', cr.dbname, attachments[0].store_fname)
+                elif len(parts) > 2 and parts[1] == 'static':
+                    # /modules/<module>/static
+                    value = s3_pool.get_url('modules', value[1:])
+
+        return super(QWeb, self).render_attribute(element, name, value, qwebcontext)
---
+++ addons/odooku/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+{
+  'name': 'Odooku',
+  'description': 'Odooku compatibility soft patches',
+  'version': '0.1',
+  'category': 'Hidden',
+  'author': 'Raymond Reggers',
+  'depends': ['base'],
+  'data': [],
+  'auto_install': True
+}
