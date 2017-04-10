c6c47d8631081d7c0deb7246c0facd7a7a1f3448
Raymond Reggers <raymond@adaptiv.nl>
2017-04-10 16:19:22 +0200

    improve cyclic dependency detection

--- odooku/data/serialization/relations.py
+++ odooku/data/serialization/relations.py
@@ -1,4 +1,4 @@
-from odooku.data.serialization.base import BaseFieldSerializer
+from odooku.data.serialization.base import BaseFieldSerializer, Dependency
 
 
 import logging
@@ -7,9 +7,10 @@ _logger = logging.getLogger(__name__)
 
 class RelationSerializer(BaseFieldSerializer):
 
-    def __init__(self, field_name, relation):
+    def __init__(self, field_name, relation, required):
         self._field_name = field_name
         self._relation = relation
+        self._required = required
 
     def serialize(self, record, context):
         return self.serialize_relation(record, context)
@@ -26,28 +27,32 @@ class RelationSerializer(BaseFieldSerializer):
     @classmethod
     def factory(cls, field_name, field, config):
         relation = field['relation']
+        required = field['required']
         model_config = config.models.get(relation, None)
         if model_config and model_config.nk or not(
                     config.includes and relation not in config.includes
                     or config.excludes and relation in config.excludes
                 ):
-            return cls(field_name, relation)
+            return cls(field_name, relation, required)
+
+    def __repr__(self):
+        return self._field_name
 
 
 class ManyToOneSerializer(RelationSerializer):
 
     def resolve_dependencies(self, context):
         serializer = context.serializers[self._relation]
-        dependencies = set([self._relation])
+        dependencies = [Dependency(self._relation, self)]
         if (context.stack[-1] != self._relation):
-            dependencies |= serializer.resolve_nk_dependencies(context)
+            dependencies += serializer.resolve_nk_dependencies(context)
         return dependencies
 
     def serialize_relation(self, record, context):
         value = record.read([self._field_name])[0][self._field_name]
         if value:
             serializer = context.serializers[self._relation]
-            context.add_relation(self._relation, value[0])
+            context.add_dependency(self._relation, value[0], self)
             return serializer.serialize_id(value[0], context)
         return False
 
@@ -68,7 +73,7 @@ class ManyToManySerializer(RelationSerializer):
             if value:
                 serializer = context.serializers[self._relation]
                 for id in value:
-                    context.add_relation(self._relation, id)
+                    context.add_dependency(self._relation, id, self)
                     result.append(serializer.serialize_id(id, context))
             return result
         else:
