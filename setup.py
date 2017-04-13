d2c0ac58952f72ec9b3489e8b4a3e0a19d76aed4
Raymond Reggers <raymond@adaptiv.nl>
2017-04-13 14:26:11 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odooku',
-    version='10.0.1006',
+    version='10.0.1007',
     url='https://github.com/adaptivdesign/odooku-compat',
     author='Raymond Reggers - Adaptiv Design',
     author_email='raymond@adaptiv.nl',
